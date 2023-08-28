6825cca24dc3fab44342fa5295ec869a6999a7f1
targinu <targino26@outlook.com>
2023-08-27 22:05:42 -0300

    adiciona audio no pulo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public float moveSpeed = 1.0f; // Velocidade de movimento
-    public float jumpForce = 7.0f;
+    public float moveSpeed = 5.0f; //velocidade de movimento
+    public float jumpForce = 5.0f; //altura do pulo
+
+    public AudioClip slimeJump; //referencia ao som de pulo
+
+    private AudioSource audioSource; //componente AudioSource
+
+    private bool isGrounded = false; //indica se o jogador está no chão
 
     private bool isMoving = false;
     private Vector3 targetPosition;
@@ -12,17 +18,35 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         targetPosition = transform.position;
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
+
+        //verifica se o jogador está no chão
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+
         if (!isMoving)
         {
-            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-            if (input != Vector3.zero)
+            // Verifica se o jogador está tentando se mover apenas na horizontal ou vertical
+            if (horizontalInput != 0 && verticalInput == 0)
             {
-                targetPosition = transform.position + input;
+                Vector3 direction = new Vector3(horizontalInput, 0, 0).normalized;
+                targetPosition = transform.position + direction;
+
+                // Aplica o pulo ao movimento (adiciona força no eixo Y)
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+                isMoving = true;
+            }
+            else if (horizontalInput == 0 && verticalInput != 0)
+            {
+                Vector3 direction = new Vector3(0, 0, verticalInput).normalized;
+                targetPosition = transform.position + direction;
 
                 // Aplica o pulo ao movimento (adiciona força no eixo Y)
                 rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -38,12 +62,23 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
 
+            //atualiza a posição do jogador usando Rigidbody
             rb.MovePosition(newPosition);
 
+            //verifica se chegou à posição de destino
             if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
             {
+                //atualiza a posição de destino para a posição atual
                 targetPosition = transform.position;
+
+                //encerra o movimento
                 isMoving = false;
+
+                //toca o som de pulo se o jogador estiver no chão
+                if (isGrounded && audioSource != null && slimeJump != null)
+                {
+                    audioSource.PlayOneShot(slimeJump);
+                }
             }
         }
     }
